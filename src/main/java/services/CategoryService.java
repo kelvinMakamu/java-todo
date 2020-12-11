5bf25d902c15cc74f06436b2c3ad997b781f5e52
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-12-11 21:09:15 +0300

    Added CategoryService class to connect to the database and interact with the data

---
+++ src/main/java/services/CategoryService.java
@@ -0,0 +1,85 @@
+package services;
+
+import dao.CategoryDao;
+import models.Category;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class CategoryService implements CategoryDao {
+    private final Sql2o sql2o;
+
+    public CategoryService(Sql2o sql2o){
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public List<Category> getAll() {
+        String query = "SELECT * FROM categories";
+        try(Connection connection = sql2o.open()){
+            return connection.createQuery(query)
+                    .executeAndFetch(Category.class);
+        }
+    }
+
+    @Override
+    public void add(Category category) {
+        String query = "INSERT INTO categories(name) VALUES(:name)";
+        try(Connection connection = sql2o.open()){
+            int id = (int)connection.createQuery(query,true)
+                    .bind(category)
+                    .executeUpdate().getKey();
+            category.setId(id);
+        }catch(Sql2oException ex){
+            System.out.println("Database Error "+ex.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public Category findById(int id) {
+        String query = "SELECT * FROM categories WHERE id=:id";
+        try(Connection connection = sql2o.open()){
+            return connection.createQuery(query)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(Category.class);
+        }
+    }
+
+    @Override
+    public void update(int id, String name) {
+        String query = "UPDATE categories SET name=:name WHERE id=:id";
+        try(Connection connection = sql2o.open()){
+            connection.createQuery(query)
+                    .addParameter("id",id)
+                    .addParameter("name",name)
+                    .executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println("Database Error "+ex.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void deletedById(int id) {
+        String query = "DELETE * FROM tasks WHERE id=:id";
+        try(Connection connection = sql2o.open()){
+            connection.createQuery(query)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        }catch (Sql2oException ex){
+            System.out.println("Database Error "+ex.getLocalizedMessage());
+        }
+    }
+
+    @Override
+    public void clearAllCategories() {
+        String query = "DELETE * FROM tasks";
+        try(Connection connection = sql2o.open()){
+            connection.createQuery(query)
+                    .executeUpdate();
+        }catch(Sql2oException ex){
+            System.out.println("Database Error "+ex.getLocalizedMessage());
+        }
+    }
+}
