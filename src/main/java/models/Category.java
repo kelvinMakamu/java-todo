ccb0a21940cc1bf6364535b13c8497da38af7e8d
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-12-11 20:50:40 +0300

    Added Category Model class

---
+++ src/main/java/models/Category.java
@@ -0,0 +1,42 @@
+package models;
+
+import java.util.Objects;
+
+public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name){
+        this.id   = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Category)) return false;
+        Category category = (Category) o;
+        return getId() == category.getId() && Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName());
+    }
+}
