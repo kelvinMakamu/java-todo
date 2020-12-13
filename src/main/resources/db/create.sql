42f41331bd9b2c05971fd3a251f7b444ca53caf8
Kelvin Makamu <makamu3pmkelvin@gmail.com>
2020-12-13 20:58:51 +0300

    Added drop.sql for droppping database tables

--- src/main/resources/db/create.sql
+++ src/main/resources/db/create.sql
@@ -1,13 +1,5 @@
-SET MODE PostgreSQL;
-
-CREATE TABLE IF NOT EXISTS tasks (
-  id int PRIMARY KEY auto_increment,
-  description VARCHAR,
-  completed BOOLEAN,
-  categoryId INTEGER
-);
-
-CREATE TABLE IF NOT EXISTS categories(
-    id int PRIMARY KEY auto_increment,
-    name VARCHAR
-);
+CREATE DATABASE todolist;
+\c todolist;
+CREATE TABLE tasks (id SERIAL PRIMARY KEY, description VARCHAR, completed BOOLEAN, categoryid INTEGER);
+CREATE TABLE categories (id SERIAL PRIMARY KEY, name VARCHAR);
+CREATE DATABASE todolist_test WITH TEMPLATE todolist;
